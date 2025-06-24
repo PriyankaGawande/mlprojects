b03fb33b7dbacf74d2455ad86daa6476cfdd1b7d
PriyankaGawande <priyanka.thakare3@gmail.com>
2025-06-24 18:21:59 -0400

    Data Ingestion

--- scr/logger.py
+++ scr/logger.py
@@ -15,7 +15,7 @@ logging.basicConfig(
 
 if __name__=="__main__":
     logging.info("logging has started")"""
-from datetime import datetime
+'''from datetime import datetime
 import os
 
 # Format the timestamp safely for file names
@@ -28,4 +28,21 @@ os.makedirs(logs_dir, exist_ok=True)
 
 # Now you can create/use the log file
 with open(logs_path, "w") as f:
-    f.write("Log initialized.\n")
+    f.write("Log initialized.\n")'''
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+
+
+)
